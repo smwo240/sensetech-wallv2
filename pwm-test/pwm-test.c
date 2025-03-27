489a840f226c78b735a40f40efd626cc2e0f6eba
cdjs10 <cst297@uky.edu>
2025-03-26 21:17:39 -0400

    tested 4 simultaneous PWM outputs for the vibration module - GP16 and GP0 share a channel so I had to move my GPIO to different pins

--- pwm-test/pwm-test.c
+++ pwm-test/pwm-test.c
@@ -1,96 +1,101 @@
-/**
-* PWM test
-* Output PWM signals on pins 0 and 1
- */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include "pico/stdlib.h"
+#include "hardware/uart.h"
+//#include "hardware/i2c.h"
 #include "hardware/pwm.h"
 #include "hardware/adc.h"
 #include "hardware/clocks.h"
-#include <stdio.h>
 
-int main() {
-    // Turn default LED on to show power is on
-    /*const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 1);
+#define INPUT_POT_ADC 26
+#define OUTPUT_LED_GPIO 25
+#define OUTPUT_MOTORCTRL_GPIO 16
+#define MODE_SEL_GPIO 19
+
+#define LED1_GPIO 1
+#define LED2_GPIO 2
+#define LED3_GPIO 3
+
+int main()
+{
+    int state, period; 
+    uint16_t result;
+
+    // mode select connected to GP19 and GND - closed circuit connects to gnd, open is vcc
+    gpio_init(MODE_SEL_GPIO);
+    gpio_set_dir(MODE_SEL_GPIO, GPIO_IN);
+    gpio_pull_up(MODE_SEL_GPIO); // open - 1, closed - 0
+    
+    // PWM GPIO
+    gpio_set_function(LED1_GPIO, GPIO_FUNC_PWM);
+    gpio_set_function(LED2_GPIO, GPIO_FUNC_PWM);
+    gpio_set_function(LED3_GPIO, GPIO_FUNC_PWM);
+    // Output to motor circuit connected to GPIO16, VCC, GND
+    // PWM enabled to control speed of motor - goes into high speed transistor so control VCC input to motor
+    gpio_set_function(OUTPUT_MOTORCTRL_GPIO, GPIO_FUNC_PWM);
+
+    uint slice0_num = pwm_gpio_to_slice_num(0); // slice 0
+    uint slice1_num = pwm_gpio_to_slice_num(2); // slice 1
+
+    /* LED1: GPIO1, PWM Channel 0B
+       LED2: GPIO2, PWM Channel 1A
+       LED3: GPIO3, PWM Channel 1B
+       MOTOR: GPIO16, PWM Channel 0A
     */
 
-    stdio_init_all();
+    gpio_init(OUTPUT_LED_GPIO);
+    gpio_set_dir(OUTPUT_LED_GPIO, GPIO_OUT);
 
-    // ADC on GPIO 26 / ADC 0
+    // Slide potentiometer connected to VCC, GND, and GPIO26
+    // ADC on GPIO26 / ADC0
     adc_init();
-    adc_gpio_init(26);
+    adc_gpio_init(INPUT_POT_ADC);
     adc_select_input(0);
 
-    // GPIO 17 - testing digital I/O
-    gpio_init(17);
-    gpio_set_dir(17, GPIO_OUT);
-    gpio_put(17, 0);
 
-    // Tell GPIO 25 and 16 they are allocated to the PWM
-    gpio_set_function(PICO_DEFAULT_LED_PIN, GPIO_FUNC_PWM);
-    gpio_set_function(16, GPIO_FUNC_PWM);
+    // set clock divider to 4, then frequency is 125/4 = 31.25 MHz
+    // Minimize this to reduce power consumption.
+    pwm_set_clkdiv(slice0_num, 4);
+    pwm_set_clkdiv(slice1_num, 4);
 
-    // Find out which PWM slice is connected to GPIO 25, GPIO 16
-    uint slice_num = pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN);
-    uint slice_num2 = pwm_gpio_to_slice_num(16);
-    // set clock divider to 1, then frequency is 125 MHz
-    pwm_set_clkdiv(slice_num, 1);
-    pwm_set_clkdiv(slice_num2, 1);
+    // Set period number of cycles for ... Hz output
+    // period = Clk/cycle\\.
+    // [WIP]
+    period = 1250;
+    pwm_set_wrap(slice0_num, period);
+    pwm_set_wrap(slice1_num, period);
 
-    // Set period number of cycles for 100 kHz output
-    // f = 125 MHz
-    // 
-    // period= 125e6/100e3 = 1250
-    int period=1250;
-    pwm_set_wrap(slice_num, period);
-    pwm_set_wrap(slice_num2, period);
-    // Set channel A / B output high for a number of cycles
-    // PIN 25 is on channel B because its an odd number - see
-    // pico forum post
-    pwm_set_chan_level(slice_num, PWM_CHAN_B, period*0.1);
-    pwm_set_chan_level(slice_num2, PWM_CHAN_A, period*0.1);
-    // Set the PWM running
-    pwm_set_enabled(slice_num, true);
-    pwm_set_enabled(slice_num2, true);
-    // Note we could also use pwm_set_gpio_level(gpio, x) which looks up the
-    // correct slice and channel for a given GPIO.
-    double multiplier = 0.1;
-    uint16_t result;
-    uint16_t adj_result;
-    char str[50];
+    // Enable PWM running
+    //pwm_set_enabled(led1_slice_num, true);
+    //pwm_set_enabled(led2_slice_num, true);
+    pwm_set_enabled(slice0_num, true);
+    pwm_set_enabled(slice1_num, true);
 
-       while (1) { 
-      // 12-bit conversion, VREF = 3.3 V  
-      // 12-bit value -> val out of 4096
-       result = adc_read(); 
-       // adj_result = 
-      // Set PWM output duty cycle
-       pwm_set_chan_level(slice_num, PWM_CHAN_B, period*multiplier/4096);
-       pwm_set_chan_level(slice_num2, PWM_CHAN_A, period*result/4096);
+    uint16_t vals_period[6] = {0.4*period,0.6*period,0.9*period,period,0.9*period,0.6*period};
+    float led_levels[3][6] = {
+        {0, 0.10, 1, 0.15, 0, 0},
+        {0, 0, 0.10, 1, 0.15, 0},
+        {0, 0, 0, 0.10, 1, 0.15} 
+    };
 
-        // set motor on/off depending on switch state
-        if ((result/4096.0) >= .75)
-            gpio_put(17,1);
-        else if ((result/4096.0) >= .50)
-            gpio_put(17,1);
-        else if ((result/4096.0) >= .25)
-            gpio_put(17,0);
-        else
-            gpio_put(17,0);
-        // used for cycling through pwm duty cycles
-       multiplier += 0.1;
+    state = 0;
 
-       if (multiplier >= 0.9)
-        multiplier = 0.1;
+    while (1) 
+    {
+        // LED cycle
+        pwm_set_gpio_level(LED1_GPIO,led_levels[0][state]*period);
+        pwm_set_gpio_level(LED2_GPIO,led_levels[1][state]*period);
+        pwm_set_gpio_level(LED3_GPIO,led_levels[2][state]*period);
+        // Motor Cycle
+        pwm_set_gpio_level(OUTPUT_MOTORCTRL_GPIO,vals_period[state]);
 
-        sprintf(str,"%lf",multiplier);
-        printf(str);
+        //gpio_put(OUTPUT_LED_GPIO, state % 2); // debug - shows program is running
 
-       sleep_ms(100);
+        sleep_ms(400);
 
-        }; // idle loop infinite
+        state = (state + 1) % 6;
 
+    }
+    return 0;
 }
