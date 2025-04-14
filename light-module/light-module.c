e172649dbea84269771b64492fd294e99e770ea4
cdjs10 <cst297@uky.edu>
2025-04-13 21:39:11 -0400

    pasted current in-use version of light module code into this directory for proper documentation.

--- light-module/light-module.c
+++ light-module/light-module.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 #include "hardware/clocks.h"
@@ -9,9 +7,6 @@
 #include "hardware/pwm.h"
 #include "hardware/uart.h"
 #include "mp3.h"
-
-// LED PIN
-#define LED_PIN 25
 
 // LED GPIO
 #define BTN1_LED_PIN 0
@@ -24,56 +19,74 @@
 #define BTN8_LED_PIN 7
 
 // BUTTON GPIO
-#define BTN1_PIN 34
-#define BTN2_PIN 32
-#define BTN3_PIN 31
-#define BTN4_PIN 29
-#define BTN5_PIN 27
-#define BTN6_PIN 26
-#define BTN7_PIN 25
-#define BTN8_PIN 24
+#define BTN1_PIN 28
+#define BTN2_PIN 27
+#define BTN3_PIN 26
+#define BTN4_PIN 22 
+#define BTN5_PIN 21
+#define BTN6_PIN 20
+#define BTN7_PIN 19
+#define BTN8_PIN 18
 
-// Mode Select GPIO
-#define MODEBTN_PIN 9
+// MODE GPIO
+#define MODE_SEL 9
+
+// ADC
+#define ADC_CLK_DIV 1000 // temp value
 
 // When a button is pressed, it lights up and stays lit until the position variable reaches the corresponding button and
 // then the direction of the position changes. This should work for multiple buttons but we can set a limit on the number of buttons "active".
 
-// Global Variables
+// global variables
 // Button states for light module logic - activated from interrupt handler
-// This is independent of the light being lit up, this is the state that matches when a button has been pressed
+// This is idependent of the light being lit up, this is the state that matches when a button has been pressed
 // and the looping behavior is waiting for the position to match a btnN_on
 
 // one off of btn# because of initial oversights  btn# = index + 1
 bool btn_active[8] = {false, false, false, false, false, false, false, false};
 
-
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
+// increment notes as number of active buttons present
+void button_press_sound() {
+    // count active buttons
+    uint count = 0;
+    for (int i = 0; i < 8; i++) {
+        if (btn_active[i] == true)
+            count++;
+    }
+    // count should be at least 1
+    switch (count) {
+        case 1: mp3_play_sound(C5);
+        case 2: mp3_play_sound(D5);
+        case 3: mp3_play_sound(E5);
+        case 4: mp3_play_sound(F5);
+        case 5: mp3_play_sound(G5);
+        case 6: mp3_play_sound(A5);
+        case 7: mp3_play_sound(B5);
+        default: // do nothing
+    }
 }
 
 void gpio_callback(uint gpio, uint32_t events) {
     // Interrupt routines for when a button is pressed on the face plate.
-    printf("Interrupt triggered");
-if (gpio == BTN1_PIN) {
+    if (gpio == BTN1_PIN) {
     if (!btn_active[0]) {
         btn_active[0] = true;
-        mp3_play_sound(C5); // play activation sound (?)
-    }
+        mp3_play_sound(C5); // play activation sound
+        }
+        
     gpio_put(BTN1_LED_PIN, true);
 } 
 else if (gpio == BTN2_PIN) {
     if (!btn_active[1]) {
         btn_active[1] = true;
-        mp3_play_sound(D5); // play activation sound
+        mp3_play_sound(D5);   // play activation sound
     }
     gpio_put(BTN2_LED_PIN, true);
 }
 else if (gpio == BTN3_PIN) {
     if (!btn_active[2]) {
         btn_active[2] = true;
-        mp3_play_sound(E5); // play activation sound
+        mp3_play_sound(E5);// play activation sound
     }
     gpio_put(BTN3_LED_PIN, true);
 }
@@ -87,7 +100,7 @@ else if (gpio == BTN4_PIN) {
 else if (gpio == BTN5_PIN) {
     if (!btn_active[4]) {
         btn_active[4] = true;
-        mp3_play_sound(G5); // play activation sound
+        mp3_play_sound(G5);// play activation sound
     }
     gpio_put(BTN5_LED_PIN, true);
 }
@@ -101,7 +114,7 @@ else if (gpio == BTN6_PIN) {
 else if (gpio == BTN7_PIN) {
     if (!btn_active[6]) {
         btn_active[6] = true;
-        mp3_play_sound(B5); // play activation sound
+        mp3_play_sound(B5);// play activation sound
     }
     gpio_put(BTN7_LED_PIN, true);
 }
@@ -121,19 +134,17 @@ else if (gpio == BTN8_PIN) {
 
 int main()
 {
-    printf("Main begin");
     stdio_init_all();
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, true);
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-
-
-    #pragma region // GPIO INITIALIZATION
+    // initialize tfplayer
+    mp3_initialize();
+    mp3_set_volume(20);
+    mp3_query_status();
 
     // Initialize GPIO - all buttons set up for negative logic on push (pull-up resistors and interrupts on falling edge)
+
+    #pragma region gpio
+
     // BTN1_LED_PIN
     gpio_init(BTN1_LED_PIN);
     gpio_set_dir(BTN1_LED_PIN, GPIO_OUT);
@@ -143,6 +154,7 @@ int main()
     gpio_set_dir(BTN1_PIN,GPIO_IN);
     gpio_pull_up(BTN1_PIN);
     gpio_set_irq_enabled_with_callback(BTN1_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback); // enable interrupts
+
     // BTN2_LED_PIN
     gpio_init(BTN2_LED_PIN);
     gpio_set_dir(BTN2_LED_PIN, GPIO_OUT);
@@ -206,52 +218,42 @@ int main()
     gpio_pull_up(BTN8_PIN);
     gpio_set_irq_enabled(BTN8_PIN, GPIO_IRQ_EDGE_FALL, true);
 
-    // MODEBTN_PIN
-    gpio_init(MODEBTN_PIN);
-    gpio_set_dir(MODEBTN_PIN,GPIO_IN);
-    gpio_pull_up(MODEBTN_PIN);
-    // no interrupts enabled for MODEBTN_PIN
-
-    #pragma endregion 
+    #pragma endregion
 
     // loop to model simple behavior.
-    int position = 1; // position of "light" moving from button to button in circular pattern
+    uint32_t position = 1; // position of "light" moving from button to button in circular pattern
     bool clockwise = true;  // true = clockwise
                             // false = counterclockwise
 
-    // initialize tfplayer
-    mp3_initialize();
-    mp3_set_volume(20);
-    mp3_query_status();
-
     while (true) {
-        // Check for off-timing with button activation
         if (!btn_active[position]) {
             // turn off previous position 
             gpio_put(position, false);
-        }
 
-        /* ========== Move to new position =============== */
+            // do direction swap function - future implmentation
+        }
+        
         if (clockwise) { 
             position = (position + 1) % 8; }   // iterate clockwise (8 % 8 = 0)
-            else { 
+        else { 
             position = (position - 1 + 8) % 8; } // iterate counterclockwise (substitute for -1 % 8 = 7)
 
-            // light up new position
-            gpio_put(position, true);
+        // light up new position
+        gpio_put(position, true);
 
         /*========= Check if button is active at new position ========= */
         if (btn_active[position]) {
-            // play a sound
-            mp3_play_sound(BUBBLEPOP);
+            // play a sound (once configured)
 
             clockwise = !clockwise; // toggle direction
             btn_active[position] = false;
 
+            mp3_play_sound(BUBBLEPOP);
+
             // button starts ON if collision occurs
             for (int i = 1; i < 7; i++) {
                 gpio_put(position, i % 2 == 0 );
-                sleep_ms(150/(1+gpio_get(MODEBTN_PIN))); // changes with speed toggle
+                sleep_ms(150);
             }
             gpio_put(position,true); // backup turn button back on
         }
@@ -259,12 +261,17 @@ int main()
             // do nothing, continue to next loop
         }
 
-        // use toggle switch for speed select
+        // Mode control 
+        // SLOW/QUIET MODE
+        if (gpio_get(MODE_SEL)) {
+            sleep_ms(500);
+            mp3_set_volume(10);
+        }
+        // FAST/LOUDER MODE
+        else  {
+            sleep_ms(400);
+            mp3_set_volume(16);
+        }
 
-         if (gpio_get(MODEBTN_PIN)) // speed toggle
-            sleep_ms(200);
-         else
-            sleep_ms(400); // speed of the rotation pattern
     }
-
 }
